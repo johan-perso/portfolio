c0ecd4b73721b7b54bd45b3774b9ab69b033c402
Johan <johan@johanstick.fr>
2026-02-11 22:06:18 +0100

    Compile contents when starting web server if not already done

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-var path = require("path")
-var roc = require("roc-framework")
+const fs = require("fs")
+const path = require("path")
+const childProcess = require("child_process")
+const roc = require("roc-framework")
+
+const contentDir = {
+	base: path.join(__dirname, "content"),
+	raw: path.join(__dirname, "content", "raw"),
+	compiled: path.join(__dirname, "content", "compiled"),
+	attachments: path.join(__dirname, "content", "attachments"),
+}
+const publicAssetsPath = "/medias/content/"
 
 // Main function, prepare and start the server
 async function main(){
 	var perfNow = performance.now()
 	console.log("Preparing the server...")
 
-	// var allPromises = []
-
-	// if(rebuildWebsite) allPromises.push(new Promise(async (resolve) => {
-	// 	if(fs.existsSync(buildedSitePath)) fs.rmSync(buildedSitePath, { recursive: true })
-	// 	await buildSite()
-	// 	resolve()
-	// }))
+	if(!fs.existsSync(contentDir.compiled)) {
+		console.log("Compiled content not found. Compiling content...")
+		var spawnedCompileProcess = childProcess.exec(`${process.versions.bun ? "bun run" : "node"} scripts/compileContent.js`)
+		await new Promise((resolve, reject) => {
+			spawnedCompileProcess.stdout.on("data", data => {
+				if(typeof data == "string") console.log(`> ${data.trim().split("\n").join("\n> ")}`)
+				else process.stdout.write(data)
+			})
+			spawnedCompileProcess.stderr.on("data", data => {
+				if(typeof data == "string") console.error(`> ${data.trim().split("\n").join("\n> ")}`)
+				else process.stderr.write(data)
+			})
+			spawnedCompileProcess.on("close", code => {
+				if(code === 0) resolve()
+				else reject(new Error(`Content compilation process exited with code ${code}`))
+			})
+		})
+	}
 
 	console.log(`Starting the server...          (took ${(Math.round(performance.now() - perfNow) / 1000).toFixed(3)}s)`)
 	startRocServer()
 }
-
-// function readFilesRecursively(dir){
-// 	var files = fs.readdirSync(dir)
-// 	var filesList = []
-
-// 	files.forEach(file => {
-// 		var filePath = path.join(dir, file)
-// 		if(fs.statSync(filePath).isDirectory()) filesList.push(...readFilesRecursively(filePath))
-// 		else filesList.push(filePath)
-// 	})
-
-// 	return filesList
-// }
 
 async function startRocServer(){
 	var server = new roc.server({
