f9be7f0ea92463e72d3e9ab0f39c7b38986d4b7b
Johan <johan@johanstick.fr>
2026-02-12 22:25:05 +0100

    Support redirections from sheet (roc-framework@4.1.0)

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ const contentDir = {
 }
 const publicAssetsPath = "/medias/content/"
 
+const contentFiles = {
+	redirections: {}
+}
+
 // Main function, prepare and start the server
 async function main(){
 	var perfNow = performance.now()
 	console.log("Preparing the server...")
 
+	// Check for compiled content, compile it if not found
 	if(!fs.existsSync(contentDir.compiled)) {
 		console.log("Compiled content not found. Compiling content...")
 		var spawnedCompileProcess = childProcess.exec(`${process.versions.bun ? "bun run" : "node"} scripts/compileContent.js`)
@@ -34,6 +39,10 @@ async function main(){
 			})
 		})
 	}
+
+	// Add some compiled content files to memory
+	contentFiles.redirections = JSON.parse(fs.readFileSync(path.join(contentDir.compiled, "redirections.json"), "utf-8"))
+	if(!contentFiles.redirections || typeof contentFiles.redirections != "object") throw new Error("Redirections content file is not valid. Found:", contentFiles.redirections)
 
 	console.log(`Starting the server...          (took ${(Math.round(performance.now() - perfNow) / 1000).toFixed(3)}s)`)
 	startRocServer()
@@ -56,14 +65,29 @@ async function startRocServer(){
 
 	var cacheControlHeader = server.isDev ? "no-cache" : "max-age=7200" // 7200sec = 2h
 
+	server.registerRoutes(contentFiles.redirections.keyValue.map(redirection => {
+		return {
+			method: "get",
+			path: redirection.origine,
+			options: {
+				redirect: redirection.destination
+			}
+		}
+	}))
+
 	server.on("request", (req, res) => {
 		if(req.method == "GET" && req.path == "/version") return res.send(200, mainVersion, { headers: { "Content-Type": "text/plain" } })
+
+		// Redirections from content file, should not be used but still here as a fallback
+		var foundRedirection = contentFiles?.redirections?.keyValue?.find(r => r?.origine == req?.path || r?.origine == `${req?.path}/` || `/${r?.origine}` == req?.path)
+		if(req.method == "GET" && foundRedirection) return res.redirect(302, foundRedirection.destination)
 
 		if(res.initialAction.type == "sendHtml") res.send(200, res.initialAction.content, { headers: { "Content-Type": "text/html", "Cache-Control": cacheControlHeader } })
 		else if(res.initialAction.type == "sendJs") res.send(200, res.initialAction.content, { headers: { "Content-Type": "application/javascript", "Cache-Control": cacheControlHeader } })
 		else if(res.initialAction.type == "sendFile") res.sendFile(200, res.initialAction.content, { headers: { "Cache-Control": [".png", ".jpg", ".svg", ".webp", ".gif", ".mp4", ".ttf", ".woff2", ".json", ".css", ".js"].includes(path.extname(req.path)) ? cacheControlHeader : "no-cache" } })
 		else if(res.initialAction.type == "redirect") res.redirect(302, res.initialAction.content)
-		else if(res.initialAction.type == "404") res.send404()
+		else if(res.initialAction.type == "404") res.redirect(302, "/")
+		else res.send(500, "Internal Server Error", { headers: { "Content-Type": "text/plain" } })
 	})
 
 	server.start()
