ed0a03b2f8d6c7f1650f91b591dc71b289d8e581
Johan <johan@johanstick.fr>
2026-02-17 23:47:04 +0100

    Lint + tailwind parse over scripts and utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./public/**/*.{html,js,json}'],
+	content: ["./public/**/*.{html,js,json}", "./scripts/**/*.{html,js,json}", "./utils/**/*.{html,js,json}"],
 	plugins: [],
 	daisyui: {
 		logs: false
@@ -8,22 +8,22 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				'header': ['Schibsted Grotesk', 'system-ui', 'sans-serif'],
-				'body': ['Bricolage Grotesque', 'system-ui', 'sans-serif'],
+				"header": ["Schibsted Grotesk", "system-ui", "sans-serif"],
+				"body": ["Bricolage Grotesque", "system-ui", "sans-serif"],
 			},
 			colors: {
-				'primary-background': '#FFFFFF',
-				'primary-header': '#181818',
-				'primary-content': '#383838',
-				'primary-content-heavy': '#1B1B1B',
-				'primary-content-light': '#5E6269',
+				"primary-background": "#FFFFFF",
+				"primary-header": "#181818",
+				"primary-content": "#383838",
+				"primary-content-heavy": "#1B1B1B",
+				"primary-content-light": "#5E6269",
 
-				'light-background': '#F8F8F8',
-				'light-background-heavy': '#F1F1F1', // 60% #F8F8F8
-				'light-background-heavier': '#E7E7E7',
-				'light-content': '#98A2B3',
+				"light-background": "#F8F8F8",
+				"light-background-heavy": "#F1F1F1", // 60% #F8F8F8
+				"light-background-heavier": "#E7E7E7",
+				"light-content": "#98A2B3",
 
-				'link': '#2563EB',
+				"link": "#2563EB",
 			}
 		},
 	},
